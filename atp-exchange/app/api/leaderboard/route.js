f7ddb71dd687a21baae66f4599d9dd9204f772a3
ryanjhallak <ryanjhallak@gmail.com>
2026-07-15 14:08:59 -0400

    Delete atp-exchange directory

--- atp-exchange/app/api/leaderboard/route.js
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-import { getLeaderboard } from "../../../lib/db";
-
-export const dynamic = "force-dynamic";
-
-export async function GET() {
-  return NextResponse.json({ leaderboard: getLeaderboard(10) });
-}
